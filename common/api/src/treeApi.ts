d02228a021c8785e0a886e013d929d326cffef39
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-05-26 18:46:00 -0700

    parse and normalize json format mock data
    - convert nested child nodes to pointers
    - add parent node pointers
    - output normalized node lookup object, and array of root note ids.

---
+++ common/api/src/treeApi.ts
@@ -0,0 +1,31 @@
+import type { IRawTreeNode, ITreeNode, UUID } from '@filetree/feature'
+import { v4 as uuid } from 'uuid'
+import mockData from './mock-data.json'
+
+export const normalizedNodes: Record<UUID, ITreeNode> = {}
+
+export function processTree(node: IRawTreeNode, isRoot = false): ITreeNode {
+  const id = uuid() as UUID
+  if (isRoot) node.parent = null
+  const processedNode =
+    node.kind === 'file'
+      ? { ...node, id }
+      : {
+          ...node,
+          id,
+          children: node.children
+            .map((child: IRawTreeNode) => {
+              const newNode = processTree(child)
+              newNode.parent = id
+              return newNode
+            })
+            .map((child: ITreeNode) => child.id),
+        }
+  normalizedNodes[processedNode.id] = processedNode
+  return processedNode
+}
+
+const data = Array.isArray(mockData) ? [...mockData] : [{ ...mockData }]
+export const rootIds: UUID[] = data.map(
+  (rootNode) => processTree(rootNode as IRawTreeNode, true).id
+)
